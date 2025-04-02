8037ca9206b91874a0f48c7b7f9bce249cb72dce
Nguyễn Trung Ánh <anhnon0106@gmail.com>
2025-04-02 22:56:51 +0700

    BE/feat/Change security

--- src/main/java/com/backend/domicare/configuration/SecurityConfiguration.java
+++ src/main/java/com/backend/domicare/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.backend.domicare.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -18,13 +19,40 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) 
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**","/files/**","/swagger-ui.html").permitAll()
-                            .requestMatchers("/login","/register","/refresh-token","/email/**","/verify-email","/oauth2/**","/forgot-password","/reset-password").permitAll()
-                            .requestMatchers("/users","/users/**").hasAnyRole("ADMIN","USER")
-                            .requestMatchers("/email").hasRole("ADMIN")
-                            .requestMatchers("/permissions","/permissions/**").hasAnyRole("ADMIN","USER")
+                            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**","/files/**","/swagger-ui.html")
+                            .permitAll()
+
+                            .requestMatchers("/login","/register","/refresh-token","/verify-email","/oauth2/**","/forgot-password","/reset-password")
+                            .permitAll()
+
+                            .requestMatchers("/users","/users/**")
+                            .hasAnyRole("ADMIN","USER")
+
+                            .requestMatchers("/email")
+                            .hasRole("ADMIN")
+
+                            .requestMatchers("/permissions","/permissions/**")
+                            .hasAnyRole("ADMIN","USER")
+
+                             // Product CRUD operations
+                            .requestMatchers(HttpMethod.GET, "api/products", "/api/products/**")
+                            .permitAll()
+                            .requestMatchers(HttpMethod.POST, "/api/products","/api/products/**").hasAnyRole("ADMIN")
+                            .requestMatchers(HttpMethod.PUT, "/api/products","/api/products/**").hasAnyRole("ADMIN")
+                            .requestMatchers(HttpMethod.DELETE, "/api/products","/api/products/**").hasAnyRole("ADMIN")
+
+                            // Category
+                            .requestMatchers(HttpMethod.GET, "api/categories", "/api/categories/**")
+                            .permitAll()
+                            .requestMatchers(HttpMethod.POST, "/api/categories","/api/categories/**").hasAnyRole("ADMIN")
+                            .requestMatchers(HttpMethod.PUT, "/api/categories","/api/categories/**").hasAnyRole("ADMIN")
+                            .requestMatchers(HttpMethod.DELETE, "/api/categories","/api/categories/**").hasAnyRole("ADMIN")
+
+                            //Cloudinary
+                            
+
                             .anyRequest().authenticated()
                             )
             .formLogin(formLogin -> formLogin.disable())

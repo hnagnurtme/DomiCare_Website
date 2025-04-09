a57607d3df6a2b750c1f034e4e996d6719a13dc3
Nguyễn Trung Ánh <anhnon0106@gmail.com>
2025-04-09 18:25:40 +0700

    BE/feat/Image on category

--- src/main/java/com/backend/domicare/service/CategoryService.java
+++ src/main/java/com/backend/domicare/service/CategoryService.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.domain.Specification;
 
 import com.backend.domicare.dto.CategoryDTO;
 import com.backend.domicare.dto.paging.ResultPagingDTO;
+import com.backend.domicare.dto.request.AddCategoryRequest;
 import com.backend.domicare.model.Category;
 public interface  CategoryService {
-    public CategoryDTO addCategory(CategoryDTO category);
+    public CategoryDTO addCategory(AddCategoryRequest categoryDTO);
     public void deleteCategory(Long id);
     public CategoryDTO fetchCategoryById(Long categoryId);
     public CategoryDTO updateCategory(Long id, CategoryDTO category);
     public ResultPagingDTO getAllCategories(Specification<Category> spec,Pageable pageable);
+
 }
